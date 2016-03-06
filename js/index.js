34c489cb7873c06883e03367fe2b7a81da7beb7c
ktran031 <ktran031@gmail.com>
2016-03-06 12:10:16 -0800

    3

--- js/index.js
+++ js/index.js
@@ -4,38 +4,43 @@ $(document).ready(function()
 });
 */
 
-$(document).ready(function(){
-    $("a").click(function(event){
-        event.preventDefault();
-    });
-});
+// $(document).ready(function(){
+// //     $("a").click(function(event){
+// //         event.preventDefault();
+// //     });
+// // });
 
+  $(document).ready(pageReady);
+    function pageReady() {
 
 $(".readmore").click(readSlide);
-$(".readless").click(lessSlide);
-
-
 
 function readSlide () {
-	$("#show-this-on-click").slideDown(); 
+	event.preventDefault();
+	$("p").slideDown(); 
 	$("#show-this-on-click").show(); 
 	$(".readmore").hide();
 	$(".readless").show();
 
 }
 
+$(".readless").click(lessSlide);
+
 function lessSlide() {
+	event.preventDefault();
 	$("#show-this-on-click").slideUp(); 
 	$(".readless").hide();
 	$(".readmore").show();
 	
 }
 
-
 $(".learnmore").click(learnSlide);
 
 function learnSlide () {
+	event.preventDefault();
 	$("#learnmoretext").slideDown();
 	$(".learnmore").hide();
+
+}
 
 }
